86428088bdd21df39d7ae3660d5bdc535d3dfcc9
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-08-10 14:46:38 +0200

    made secret to a variable

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -5,7 +5,7 @@ export default NuxtAuthHandler({
   pages: {
     signIn: "/login",
   },
-  secret: "7kzwa#9P#w@*gq@!%svy9WpPsNPqoF2h#mySv%2cvYK6x&o$*c8XuoWx@cYGWw8yZERBS^$5o@Q5Hh&3ZRaX%#^6Un#t#XY!xHD",
+  secret: process.env.SECRET,
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({
