33314788b3558e9776c5bf9d6fe18679e20ad4e9
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-08-12 16:12:00 +0200

    added redirect to main page if unauthenticated

--- pages/login.vue
+++ pages/login.vue
@@ -49,6 +49,13 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://udchhbnbsgycesnvirnw.supabase.co'
 const supabase = createClient(supabaseUrl, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkY2hoYm5ic2d5Y2VzbnZpcm53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE2NjkwNTcsImV4cCI6MjAwNzI0NTA1N30.WRu7wJHYrVXcUadE-sFUT4YSYWq-kDnQe98s3GMeFMk")
 
+
+const { data: { user } } = await supabase.auth.getUser()
+
+if(user){
+  navigateTo("/")
+}
+
 const state = ref({
   email: undefined
 })
@@ -60,7 +67,6 @@ const validate = (state: any): FormError[] => {
 }
 
 const form = ref()
-
 
 async function submit() {
   await form.value!.validate()
